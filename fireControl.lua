906d06f113f345b16a643c5e9f0e08fc8c944e4a
prodigylock <prodigylock@gmail.com>
2023-08-31 02:59:00 +0300

    cannon gro brrrrr

--- fireControl.lua
+++ fireControl.lua
@@ -2,6 +2,15 @@ print("Obtaining peripherals")
 local rs1 = peripheral.wrap("redstoneIntegrator_7")
 local rs2 = peripheral.wrap("redstoneIntegrator_8")
 local rs3 = peripheral.wrap("redstoneIntegrator_9")
+local rs4 = peripheral.wrap("redstoneIntegrator_10")
+
+local yawControl = peripheral.wrap("scrollBehaviourEntity_2")
+local pitchControl = peripheral.wrap("scrollBehaviourEntity_3")
+
+yawControl.setTargetSpeed(0)
+pitchControl.setTargetSpeed(0)
+
+yawControl.setOutput()
 
 local tick = 1/20
 
@@ -27,13 +36,42 @@ local function loadShot()
         os.sleep(1)
     end
     loadAction()
+
+    --reset carriage signal
     rs1.setOutput("right",true)
     os.sleep(tick)
     rs1.setOutput("right",false)
 
+    --reload signal
     rs3.setOutput("back",true)
     os.sleep(tick)
     rs3.setOutput("back",false)
+
+
+
+
+
+end
+
+local function takeAim()
+    --lift weapon
+    rs4.setOutput("left",true)
+    os.sleep(2)
+    --assemble
+    rs3.setOutput("top",true)
+    --take aim
+    os.sleep(3)
+    --fire
+    rs3.setOutput("left",true)
+    os.sleep(tick)
+    rs3.setOutput("left",false)
+    os.sleep(tick)
+    --disassemble
+    rs3.setOutput("top",false)
+    os.sleep(tick)
+    --lower
+    rs4.setOutput("left",false)
+    os.sleep(2)
 end
 
 for i = 1, 10, 1 do
@@ -41,3 +79,4 @@ for i = 1, 10, 1 do
     os.sleep(1)
 end
 loadShot()
+takeAim()
