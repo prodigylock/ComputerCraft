12cc8028ee02f531a44e84196e084fbee8da9563
prodigylock <prodigylock@gmail.com>
2023-08-31 06:25:22 +0300

    new architecture

--- fireControl.lua
+++ fireControl.lua
@@ -3,6 +3,7 @@ local rs1 = peripheral.wrap("redstoneIntegrator_7")
 local rs2 = peripheral.wrap("redstoneIntegrator_8")
 local rs3 = peripheral.wrap("redstoneIntegrator_9")
 local rs4 = peripheral.wrap("redstoneIntegrator_10")
+local rs5 = peripheral.wrap("redstoneIntegrator_11")
 
 local yawControl = peripheral.wrap("scrollBehaviourEntity_2")
 local pitchControl = peripheral.wrap("scrollBehaviourEntity_3")
@@ -28,7 +29,7 @@ end
 local function loadShot()
     for i = 1, 5, 1 do
         loadAction()
-        rs1.setOutput("left",true)
+        rs1.setOutput("left",true) --carriage signal
         os.sleep(tick)
         rs1.setOutput("left",false)
         os.sleep(1)
@@ -36,9 +37,9 @@ local function loadShot()
     loadAction()
 
     --reset carriage signal
-    rs1.setOutput("right",true)
+    rs1.setOutput("front",true)
     os.sleep(tick)
-    rs1.setOutput("right",false)
+    rs1.setOutput("front",false)
 
     --reload signal
     rs3.setOutput("back",true)
@@ -52,13 +53,31 @@ local function loadShot()
 end
 
 local function takeAim()
+
+    --screw
+    --new screw sequence
+        --move out ram
+        rs5.setOutput("bottom",true)
+        os.sleep(tick)
+        rs5.setOutput("bottom",false)
+        os.sleep(0.5)
+        --move in screw
+        rs5.setOutput("top",true)
+        os.sleep(tick)
+        rs5.setOutput("top",false)
+        os.sleep(0.5)
+
+        --screw
+        rs5.setOutput("back",true)
+        os.sleep(tick)
+        rs5.setOutput("back",false)
+        os.sleep(0.5)
+
+
     --lift weapon
     rs4.setOutput("left",true)
     os.sleep(2)
 
-    --screw
-    rs2.setOutput("front",true)
-    os.sleep(1)
 
     --assemble
     rs3.setOutput("top",true)
@@ -74,18 +93,69 @@ local function takeAim()
     rs3.setOutput("top",false)
     os.sleep(tick)
 
-    --unscrew
-    rs2.setOutput("front",false)
-    os.sleep(1)
-
     --lower
     rs4.setOutput("left",false)
     os.sleep(2)
+
+    --unscrew
+        --unscrew
+        rs5.setOutput("back",true)
+        os.sleep(tick)
+        rs5.setOutput("back",false)
+        os.sleep(0.5)
+
+        --move out screw
+        rs5.setOutput("top",true)
+        os.sleep(tick)
+        rs5.setOutput("top",false)
+        os.sleep(0.5)
+
+        --move in ram
+        rs5.setOutput("bottom",true)
+        os.sleep(tick)
+        rs5.setOutput("bottom",false)
+        os.sleep(0.5)
+end
+
+local function unfold()
+    --open bay doors
+
+    --unfold screw
+    rs5.setOutput("top",true)
+    os.sleep(tick)
+    rs5.setOutput("top",false)
+    os.sleep(0.5)
+
+    -- move in loader
+    rs5.setOutput("bottom",true)
+    os.sleep(tick)
+    rs5.setOutput("bottom",false)
+    os.sleep(0.5)
+end
+
+local function fold()
+    -- move out ram
+    rs5.setOutput("bottom",true)
+    os.sleep(tick)
+    rs5.setOutput("bottom",false)
+    os.sleep(0.5)
+
+    --fold in screw
+    rs5.setOutput("top",true)
+    os.sleep(tick)
+    rs5.setOutput("top",false)
+    os.sleep(0.5)
+
+    -- close bay doors
+
 end
 
 for i = 1, 10, 1 do
     print(i)
     os.sleep(1)
 end
+
+unfold()
 loadShot()
 takeAim()
+fold()
